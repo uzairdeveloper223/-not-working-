8ffc1108a7be2d9e19b4d8ec1ef8f2f42d92cf11
Uzair Mughal <151128570+uzairdeveloper223@users.noreply.github.com>
2025-03-12 05:11:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -141,5 +141,5 @@ app.get('/sendUZT', async (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
